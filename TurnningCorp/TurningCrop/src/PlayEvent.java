623cc64badf14c561b52c0976e3c420b82de609e
bee-p <weileenw@naver.com>
2021-08-28 22:45:00 +0900

    Add: mapObject 출력 템플릿, 1층 방1 이벤트 메소드 내용 추가
    오브젝트의 경우 출력 템플릿을 만들어 좀 더 출력 코드를 간소화시킴.
    1층 방1의 코드는 대부분 완성하였으나, 코드 구조나 세부사항에 대해서는
    추가 및 수정이 필요함.

--- TurnningCorp/TurningCrop/src/PlayEvent.java
+++ TurnningCorp/TurningCrop/src/PlayEvent.java
@@ -5,17 +5,22 @@ import java.util.Scanner;
  * object 안의 item: 실제 아이템... (책상 위의 메모지, 일기장, 사탕 껍데기 ,,,)
  */
 
+/*
+ * 오기입 관련해서는 익셉션 처리로 넘길 수도 있음...아직 고민중
+ * 또한 일정 부분 메소드로 따로 만들어 처리할 수도 있음(반복되는 부분)
+ */
 
 public class PlayEvent {
-	// 스캐너 객체, 선택지 변수 생성
+	// 스캐너 객체, 선택지/오브젝트 임시 저장 변수 생성
 	private Scanner scan = new Scanner(System.in);
 	private int num;
+	private MapObject mapObject;
 	
 	// 타이틀 이동 판별
 	private boolean goTitle;
 	
 	// 엔딩 판별할 때 사용하는 포인트 값
-	int happyEndingPoint = 0;
+	private int endingPoint = 0;
 	
 	// Player, Map 객체
 	// -> GameManager 클래스에서 PlayEvent 객체 생성할 때 집어넣기
@@ -105,6 +110,16 @@ public class PlayEvent {
 				continue;
 			}
 		}
+	}
+	
+	// 사물(object 출력 템플릿)
+	// 해당 오브젝트를 받아와 출력함
+	public void objectPrint(MapObject mapObject)
+	{
+		// ex) 부장님의 책상이다.
+		System.out.println(mapObject.getObjectName() + "이다.");
+		// 해당 오브젝트의 설명 출력
+		System.out.println(mapObject.getDescription());
 	}
 	
 	
@@ -215,20 +230,35 @@ public class PlayEvent {
 			
 			num = scan.nextInt();
 			
+			// 선택지 안의 번호를 제대로 입력하지 않았을 경우(오기입)
+			if (!(num >= 1 && num <= map[0][1].getAllObject().length + 1))
+			{
+				System.out.println("그런 선택지는 없다.");
+				continue;
+			}
+			
+			// 로비로 나가기를 선택했을 경우
+			if (num == map[0][1].getAllObject().length + 1)
+			{
+				// 로비로 위치 재조정
+				player.setPosID(10);
+				break;						// 반복문 탈출! (메소드 종료)
+			}
+			
+			
+			// 선택지를 제대로 입력했을 경우 (기본 출력)
+			// 1. 현재 오브젝트를 선택한 오브젝트로 설정
+			mapObject = map[0][1].getObject(num - 1);
+			
+			// 2. 해당 오브젝트의 이름 & 설명 출력
+			// ex) 부장님의 책상과 그 책상에 대한 설명 출력..
+			objectPrint(mapObject);
+			
+			
 			if (num == 1)		// 부장 책상
 			{
-				// 부장님의 책상이다.
-				System.out.println(map[0][1].getObject(0).getObjectName() + "이다.");
-				// 부장 책상 설명 출력
-				System.out.println(map[0][1].getObject(0).getDescription());
-				
-				/* 
-				 * 이 부분을 따로 빼서 만드는 게 좋을지,,고민중
-				 * 만약 빼지 않는다면 그냥 "~가 있다",,의 형식으로 수정 필요
-				 */
-				
-				// 책상 위에는 메모지(이)가 있다.
-				System.out.println("\n책상 위에는 " + map[0][1].getObject(0).getItem(0).getName() + "(이)가 있다.");
+				// 책상 위에는 메모지가 있다.
+				System.out.println("\n책상 위에는 " + mapObject.getItem(0).getName() + "가 있다.");
 				
 				System.out.println("자세히 볼까?");
 				System.out.println("1. 본다.");
@@ -237,49 +267,274 @@ public class PlayEvent {
 				num = scan.nextInt();
 				
 				if (num == 1)		// 본다
-				{
-					System.out.println("'" + map[0][1].getObject(0).getItem(0).getName() + "'");
+				{	// 메모지 내용 출력
+					// 사장님 결재 서류는 이메일로 보내세요.
+					System.out.println(mapObject.getItem(0).getDescription());
 				}
 				else if (num == 2)	// 안 본다
 				{
-					System.out.println("안 보기로 했다.");
+					System.out.println("안 보기로 했다. 상사의 메모는 훔쳐보면 안 된다.");
+					System.out.println("다른 책상을 살펴보자.");
 				}
 				else				// 오기입
 				{
 					System.out.println("그런 선택지는 없다.");
 				}
-				
-				continue;
 			}
 			else if (num == 2)	// 수상한 사원의 책상
 			{
-				
+				while(true)
+				{
+					System.out.println("책상 위와 책상 아래 서랍 중 어느 곳을 먼저 볼까?");
+					System.out.println("1. 책상 위를 보자.");
+					System.out.println("2. 책상 아래 서랍을 보자.");
+					System.out.println("3. 안 볼 것이다. (책상 목록으로 나가기)\n");
+					
+					num = scan.nextInt();
+					
+					if (num == 1)		// 책상 위 조사
+					{
+						// 이미 아이템을 가져갔는지 검사(일기장)
+						if (player.searchItem(mapObject.getItem(0).getName()))
+						{
+							// 아이템을 이미 가져갔다면 다시 책상 위/책상 아래 선택지로 이동
+							System.out.println("책상 위에 더이상 특이한 건 보이지 않는다.");
+							continue;
+						}
+						
+						// 아이템을 가져가지 않았다면 기존 진행 그대로..
+						
+						// 책상 위에는 일기장이 있다.
+						System.out.println("책상 위에는 " + mapObject.getItem(0).getName() + "이 있다.");
+						System.out.println("가져갈까?");
+						System.out.println("1. 가져가자.");
+						System.out.println("2. 가져가지 말자.");
+						
+						num = scan.nextInt();
+						
+						if (num == 1)
+						{
+							// 플레이어 인벤토리에 아이템 저장
+							player.saveInventory(mapObject.getItem(0));
+							
+							System.out.println(mapObject.getItem(0).getName() + "을 가져갔다.");
+						}
+						else if (num == 2)
+						{
+							System.out.println("가져가지 않기로 했다.");
+						}
+						else				// 오기입
+						{
+							System.out.println("그런 선택지는 없다.");
+						}
+					}
+					else if (num == 2)	// 책상 아래 서랍 조사
+					{
+						// '모르는 열쇠'를 갖고 있는지 검사
+						// 갖고 있다면
+						if (player.searchItem("모르는 열쇠"))
+						{
+							System.out.print("책상 서랍 안에는 ");
+							
+							// 해당 아이템을 갖고 갔는지 검사(쉽배악1)
+							if (player.searchItem(mapObject.getItem(1).getName()))
+							{
+								System.out.println("더이상 아무 것도 없다.");
+							}
+							else	// 아직 가져가지 않았다면
+							{	
+								System.out.println(mapObject.getItem(1).getName() + "이 있다.");
+								System.out.println("가져갈까?");
+								System.out.println("1. 가져가자.");
+								System.out.println("2. 가져가지 말자.");
+								
+								num = scan.nextInt();
+								
+								if (num == 1)		// 가져가자
+								{
+									// 플레이어 인벤토리에 아이템 저장
+									player.saveInventory(mapObject.getItem(1));
+									
+									System.out.println(mapObject.getItem(1).getName() + "을 가져갔다.");
+								}
+								else if (num == 2)	// 가져가지 말자
+								{
+									System.out.println("가져가지 않기로 했다.");
+								}
+								else				// 오기입
+								{
+									System.out.println("그런 선택지는 없다.");
+								}
+							}
+						}
+						else	// 열쇠가 없다면
+						{
+							System.out.println("서랍이 열리지 않는다.");
+							System.out.println("자물쇠로 단단히 잠겨있다. 열쇠가 필요할 것 같다.");
+						}
+					}
+					else if (num == 3)	// 안 보기
+					{
+						System.out.println("아무리 수상하더라도 남의 책상을 보면 안 된다.");
+						System.out.println("수상한 사원의 책상에서 나왔다.");
+						break;		// 반복문 탈출, 책상 목록들 선택지로 이동함
+					}
+					else				// 오기입
+					{
+						System.out.println("그런 선택지는 없다.");
+					}
+				}
 			}
 			else if (num == 3)	// 어지러운 책상
 			{
+				// 서류더미 이름, 설명 출력
+				System.out.println("책상 위에는 " + mapObject.getItem(0).getName() + "가 있다.");
+				System.out.println(mapObject.getItem(0).getDescription());
 				
+				System.out.println("그 외에 별다른 것은 없어보인다.");
+				System.out.println("사실상 책상 위가 어지러워 뭐가 뭔지 알아보기가 힘들다. 나가자.");
 			}
 			else if (num == 4)	// 정돈된 책상
 			{
+				// 다이어리가 있다!
+				System.out.println("책상 위에는 " + mapObject.getItem(0).getName() + "가 있다.");
+				System.out.println("자세히 볼까?");
+				System.out.println("1. 본다.");
+				System.out.println("2. 안 본다.");
 				
+				num = scan.nextInt();
+				
+				if (num == 1)		// 본다
+				{	
+					// 다이어리의 설명 출력
+					System.out.println(mapObject.getItem(0).getDescription());
+				}
+				else if (num == 2)	// 안 본다
+				{
+					System.out.println("안 보기로 했다.");
+					System.out.println("다른 책상들을 보러가자.");
+				}
+				else				// 오기입
+				{
+					System.out.println("그런 선택지는 없다.");
+				}
 			}
 			else if (num == 5)	// 배부른 책상.....?
-			{
+			{	
+				// '사탕 껍데기' 출력
+				System.out.print(mapObject.getItem(0).getName());
 				
+				// 안 뜯은 과자봉지를 가져갔는지 검사
+				if (player.searchItem(mapObject.getItem(1).getName()))
+				{
+					// 가져갔다면 사탕 껍데기에 대한 이름/설명만 출력
+					System.out.println("가 있다.");
+					System.out.println(mapObject.getItem(0).getDescription());
+				}
+				else	// 안 가져갔다면
+				{
+					// 사탕 껍데기와 안 뜯은 과자봉지가 있다.
+					System.out.println("와 " + mapObject.getItem(1).getName() + "가 있다.");
+					
+					System.out.println("어느 것을 먼저 볼까?");
+					// 선택지 목록 출력(사탕 껍데기, 과자봉지,,,)
+					for (int i = 0; i < 2; i++)
+						System.out.println((i + 1) + ". " + mapObject.getItem(i).getName());
+//					System.out.println("3. 안 보기");	--> 선택지 번호..유동성있게 수정 필요
+					
+					num = scan.nextInt();
+					
+					if (num == 1)		// 1. 사탕 껍데기 선택
+					{
+						// 사탕 껍데기 설명 출력
+						System.out.println(mapObject.getItem(0).getDescription());
+					}
+					else if (num == 2)	// 2. 안 뜯은 과자봉지 선택
+					{
+						// 안 뜯은 과자봉지 설명 출력
+						System.out.println(mapObject.getItem(1).getDescription());
+						System.out.println("가져갈까?");
+						System.out.println("1. 가져가자.");
+						System.out.println("2. 냅두자.");
+						
+						num = scan.nextInt();
+						
+						if (num == 1)		// 가져가자
+						{
+							// 플레이어 인벤토리에 아이템 저장
+							player.saveInventory(mapObject.getItem(1));
+							
+							System.out.println(mapObject.getItem(1).getName() + "를 챙겼다.");
+						}
+						else if (num == 2)	// 냅두자
+						{
+							System.out.println("냅두기로 했다.");
+							System.out.println("다른 책상이나 둘러보자.");
+						}
+						else				// 오기입
+						{
+							System.out.println("그런 선택지는 없다.");
+						}
+					}
+					else	// 그 외 오기입
+					{
+						System.out.println("선택지를 잘못 입력한 듯 하다.");
+					}
+				}
 			}
 			else if (num == 6)	// 플레이어의 책상
 			{
+				System.out.println("책상 서랍을 열어볼까?");
+				System.out.println("1. 열어보자.");
+				System.out.println("2. 열지말자.");
 				
-			}
-			else if (num == 7)	// 로비로 나가기
-			{	
-				// 로비로 위치 재조정
-				player.setPosID(10);
-				break;
-			}
-			else 				// 오기입
-			{
+				num = scan.nextInt();
 				
+				if (num == 1)		// 서랍 열기
+				{
+					// 모르는 열쇠를 가져갔다면
+					if (player.searchItem(mapObject.getItem(0).getName()))
+					{
+						System.out.println("아무 것도 들어있지 않다. 평소에 무언가를 좀 넣어둘 걸 그랬나보다.");
+					}
+					// 아직 가져가지 않았다면
+					else
+					{	
+						// 모르는 열쇠가 들어있다.
+						System.out.println(mapObject.getItem(0).getName() + "가 들어있다. 이게 뭐지?");
+						System.out.println("가져가볼까?");
+						System.out.println("1. 가져가자.");
+						System.out.println("2. 냅두자.");
+						
+						num = scan.nextInt();
+						
+						if (num == 1)		// 가져가자
+						{
+							// 플레이어 인벤토리에 아이템 저장
+							player.saveInventory(mapObject.getItem(0));
+							
+							System.out.println(mapObject.getItem(0).getName() + "를 챙겼다.");
+						}
+						else if (num == 2)	// 냅두자
+						{
+							System.out.println("냅두기로 했다.");
+							System.out.println("다른 책상이나 둘러보자.");
+						}
+						else				// 오기입
+						{
+							System.out.println("그런 선택지는 없다.");
+						}
+					}
+				}
+				else if (num == 2)		// 서랍 열지 않기
+				{
+					System.out.println("열지 않기로 했다.");
+					System.out.println("다른 책상이나 좀 더 둘러보자.");
+				}
+				else				// 오기입
+				{
+					System.out.println("그런 선택지는 없다.");
+				}
 			}
 		}
 	}
